85866a3f66dc9f1564d425e63c486dc0d8854f0d
Jafet <febmerlib@gmail.com>
2023-03-21 19:57:07 +0000

    fix uodated fetching

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,25 +1,38 @@
-import {api} from '../../api'
+import { api } from '../../api'
 import { getServerSideSitemap } from 'next-sitemap'
 
 export const getServerSideProps = async (ctx) => {
     try {
         console.time("sitemap")
-        const {data} = await api.FetchSiteMap()
+        const resp = await fetchApi({
+            query: `query($limit:Int, $development: String!) {
+          getAllPost(limit:$limit, development:$development ){
+            results{
+              slug
+            }
+          }
+        }`,
+            variables: {
+                //limit: 5,
+                development: "diariocivitas"
+            }
+        })
+        //        const { data } = await api.FetchSiteMap()
         console.timeEnd("sitemap")
-        const fields = data?.slugs?.map(item => (
+        const fields = resp?.results?.map(item => (
             {
-                loc: `https://diariocivitas.com/${item}`,
-                changefreq : "daily",
+                loc: `https://diariocivitas.com/${item?.slug}`,
+                changefreq: "daily",
                 priority: 0.7,
                 lastmod: new Date().toISOString(),
-             }
+            }
         ))
         return getServerSideSitemap(ctx, fields)
-        
+
     } catch (error) {
-        console.log(1009,error)
+        console.log(1009, error)
         return getServerSideSitemap(ctx, [])
     }
 }
 
-export default function Site () {}
+export default function Site() { }
