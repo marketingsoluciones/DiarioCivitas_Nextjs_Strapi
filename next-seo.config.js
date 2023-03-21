8f9785c72a1b4cea83cda725cedbf9fcc2f23cdd
eduardo andres <adch.edch@gmail.com>
2023-03-22 03:09:21 +0500

    add next-seo libreria y configuracion

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'tu noticiero mas actualizado ',
+    titleTemplate: 'diariocivitas.com',
+    defaultTitle: 'diariocivitas',
+    description: 'enterate de las noticias mas actualizadas del momento, actualidad, deportes, cultura y politica todo en un mismo lugar',
+    openGraph: {
+      type: 'diariocivitas.com',
+      locale: 'es_IE',
+      url: 'https://www.diariocivitas.com/',
+      site_name: 'diariocivitas'
+    },
+  }
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import "../styles/globals.css";
 import DefaultLayout from "../layouts/DefaultLayout.js";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { DefaultSeo } from "next-seo";
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
     <DefaultLayout>
+      <DefaultSeo {...SEO}/>
       <Component {...pageProps} />
     </DefaultLayout>
   );
