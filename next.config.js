e0a708d9c6e43b30ff6885b1de4a70e57d191e9d
eduardo andres <adch.edch@gmail.com>
2023-03-22 02:22:08 +0500

    add label lang

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images: {
       domains: ['api.diariocivitas.com', '147.182.235.100', 'cdn.weatherapi.com'],
     },
+    i18n: {
+      locales: ["es"],
+      defaultLocale: "es",
+    },
   }
